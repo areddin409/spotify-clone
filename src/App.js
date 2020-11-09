1736a53cae1ec7084b7bcc2c4d7f941814c2ad7b
areddin409 <areddin409@gmail.com>
2020-11-09 15:27:56 -0600

    Created Login with Spotify Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className='app'>
-      <h1>Spotify Clone</h1>
-      {/* Spotify logo */}
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  //runs code based on a given condition
+  useEffect(() => {
+    //get access token from url
+    const hash = getTokenFromUrl();
+    //clear url
+    window.location.hash = '';
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log('🙋', user);
+      });
+    }
+  }, []);
+
+  return <div className='app'>{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
