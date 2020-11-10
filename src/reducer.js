73b1d3cff09e5d894aab6d56eb1b12ddb00d0db7
areddin409 <areddin409@gmail.com>
2020-11-10 13:03:43 -0600

    finished styling the sidebar

---
+++ src/reducer.js
@@ -0,0 +1,32 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  //TODO: REMOVE AFTER DEVELOPING....
+  token:
+    'BQAZNNtxiqccdm9E9PM8_6xix_XiULuqgRmRMnpZG1WZ8EuiRhSvD5sPsyGbDie0jrkewiSP1qnQsvIAnN34c2I60lmP3b6vwhsvs3JgaOaVyMT4jCnGVXORfhJ4f5fO_AF5i0gS7OyZsnuetBOhM2EH4QpnGT',
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  //Action -> type, [payload]
+
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
